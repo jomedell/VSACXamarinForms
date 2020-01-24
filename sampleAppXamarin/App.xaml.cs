c4ffa279a0f310221dbe1d9d5e7e7cdc503f4e65
Jorge Medellin <jorge.medellin@microsoft.com>
2020-01-24 11:18:16 -0600

    Initial check-in of module sampleAppXamarin

---
+++ sampleAppXamarin/App.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using Xamarin.Forms;
+using Microsoft.AppCenter;
+using Microsoft.AppCenter.Analytics;
+using Microsoft.AppCenter.Crashes;
+
+namespace sampleAppXamarin
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            AppCenter.Start("android=<INSERT_ANDROID_APP_SECRET_HERE>;" +
+                   "ios=<INSERT_IOS_APP_SECRET_HERE>;",
+                   typeof(Analytics), typeof(Crashes));
+            MainPage = new ButtonPage();
+        }
+    }
+}
