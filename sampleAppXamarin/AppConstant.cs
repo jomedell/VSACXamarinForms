6cd6c1bf5bd7a99ad893b26bdbece5c686030c51
jomedell <jomedell@microsoft.com>
2020-01-24 14:32:56 -0600

    feat(Build Scripts): Pre-build Script
    #5 comment:
    
    • Update script

---
+++ sampleAppXamarin/AppConstant.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace sampleAppXamarin
+{
+    public class AppConstant
+    {
+        public static string ApiBaseUrl = "localApiBaseUrl";
+        public static string SitecoreBaseURL = "localSiteCoreBaseURL";
+        public static string IdamBaseUrl = "localIdamBaseUrl";
+        public static string SubscriptionKey = "localSubscriptionKey";
+        //public static string SubscriptionKey = "localSubscriptionKey";
+    }
+}
